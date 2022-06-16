1a18ca61fc82491c5826c9e644c43cd2c8ba7b6e
DGsystem <ms0080gzv2@hotmail.com>
2022-06-16 14:34:19 +0800

    Resolution
    add Resolution function

--- Assets/Script/SettingsMenu.cs
+++ Assets/Script/SettingsMenu.cs
@@ -1,14 +1,29 @@
 using UnityEngine.Audio;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class SettingsMenu : MonoBehaviour
 {
-    public AudioMixer settedAduio;
+
+    //Set RESOLUTION 
+    List<int> widths = new List<int>(){568, 960, 1280, 1920};
+    List<int> heights = new List<int>(){320, 540, 800, 1000};
+
+    public void SetScreenSize(int index)
+    {
+        bool fullScreen = Screen.fullScreen;
+        int width = widths[index];
+        int height = heights[index];
+        Screen.SetResolution(width, height, fullScreen);
+    }
+    //END
+
     public void SetFullScreen(bool isFullScreen)
     {
         Screen.fullScreen = isFullScreen;
     }
 
+    public AudioMixer settedAduio;
     public void SetVolume(float volume)
     {
         settedAduio.SetFloat("volume", volume); //have to use on tag, value
